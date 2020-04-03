0dbbcbf2b0bf8f43160363234a5f483c1346f6df
Adam Burke <adam.burke603@gmail.com>
2020-04-03 19:11:51 -0400

    created UI, created recursive function for multiple pings

--- ping_web_app/public/js/webPingClient.js
+++ ping_web_app/public/js/webPingClient.js
@@ -4,19 +4,38 @@
 
 console.log('from external file')
 
+var sumRTL = 0;
+var numberOfPings = 0;
+
+function updateHTML(){
+    document.getElementById('report').innerHTML = 'calculating...'
+    document.getElementById('pingbutton').disabled = true;
+}
+
 function ping(){
-            
-    var startTime = new Date().getTime(); 
     console.log('start time: ' + startTime);
 
     var xhttp = new XMLHttpRequest();
-    xhttp.open("GET", "http://127.0.0.1:3000/", true);
+    xhttp.open("GET", "http://127.0.0.1:3000/", false);
     xhttp.onload = function() { 
+        var endTime = new Date().getTime()
         console.log(xhttp.responseText)
         
-        var endTime = new Date().getTime()
-        console.log('end time: ' + endTime)
-        console.log('time elapsed: ' + (endTime - startTime))
+        // console.log('end time: ' + endTime)
+        
+        var RTL = endTime - startTime;
+        console.log('time elapsed: ' + RTL);
+
+        sumRTL += RTL;
+
+        console.log(numberOfPings)
+        if(numberOfPings < 10){
+            numberOfPings++
+            setTimeout(function(){ping(); }, 1000)
+        }else{
+            document.getElementById('report').innerHTML = sumRTL/10 + " ms"
+        }
     };
+    var startTime = new Date().getTime(); 
     xhttp.send(); 
 }
